ceddfbb8d94114a5c6c1182cf4db8ba3b6868e8f
Hariharan Padmanaban <157977286+HariPad2005@users.noreply.github.com>
2025-02-23 20:10:48 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // Intentional error: Missing return type for the method
+    hello() {
+        System.out.println("This method has an error.");
+    }
+}
